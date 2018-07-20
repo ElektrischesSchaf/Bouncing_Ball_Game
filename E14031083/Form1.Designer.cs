40cc7e793d9f5c4934cf047542976a63c3e4e003
ElektrischesSchaf <vertigo1958psycho1960@gmail.com>
2018-07-20 21:49:35 +0800

    first git

---
+++ E14031083/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace E14031083
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.saber = new System.Windows.Forms.PictureBox();
+            this.ball3 = new System.Windows.Forms.PictureBox();
+            this.ball2 = new System.Windows.Forms.PictureBox();
+            this.ball1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.startbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textbox_score = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.saber)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // saber
+            // 
+            this.saber.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.saber.BackColor = System.Drawing.Color.Transparent;
+            this.saber.Image = global::E14031083.Properties.Resources._4BQaIPg;
+            this.saber.Location = new System.Drawing.Point(404, 707);
+            this.saber.Name = "saber";
+            this.saber.Size = new System.Drawing.Size(589, 54);
+            this.saber.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.saber.TabIndex = 3;
+            this.saber.TabStop = false;
+            this.saber.MouseDown += new System.Windows.Forms.MouseEventHandler(this.mouse_down);
+            this.saber.MouseMove += new System.Windows.Forms.MouseEventHandler(this.mouse_move);
+            // 
+            // ball3
+            // 
+            this.ball3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ball3.BackColor = System.Drawing.Color.Transparent;
+            this.ball3.Image = global::E14031083.Properties.Resources.Sports_Ball_Icon;
+            this.ball3.Location = new System.Drawing.Point(264, 341);
+            this.ball3.Margin = new System.Windows.Forms.Padding(0);
+            this.ball3.Name = "ball3";
+            this.ball3.Size = new System.Drawing.Size(140, 140);
+            this.ball3.TabIndex = 2;
+            this.ball3.TabStop = false;
+            // 
+            // ball2
+            // 
+            this.ball2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ball2.BackColor = System.Drawing.Color.Transparent;
+            this.ball2.Image = global::E14031083.Properties.Resources.ball_02;
+            this.ball2.Location = new System.Drawing.Point(1061, 319);
+            this.ball2.Margin = new System.Windows.Forms.Padding(0);
+            this.ball2.Name = "ball2";
+            this.ball2.Size = new System.Drawing.Size(140, 140);
+            this.ball2.TabIndex = 2;
+            this.ball2.TabStop = false;
+            // 
+            // ball1
+            // 
+            this.ball1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ball1.BackColor = System.Drawing.Color.Transparent;
+            this.ball1.Image = global::E14031083.Properties.Resources._12375609491338902396pitr_Ball_icon_svg_thumb;
+            this.ball1.Location = new System.Drawing.Point(171, 87);
+            this.ball1.Margin = new System.Windows.Forms.Padding(0);
+            this.ball1.Name = "ball1";
+            this.ball1.Size = new System.Drawing.Size(140, 140);
+            this.ball1.TabIndex = 2;
+            this.ball1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(1083, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(130, 177);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "速度";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(29, 140);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(43, 19);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "快";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.fast_checked);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(29, 100);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(43, 19);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "中";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.medium_checked);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(29, 54);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(43, 19);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "慢";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.slow_ckecked);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Location = new System.Drawing.Point(1233, 68);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(130, 159);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "球數";
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(29, 125);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(35, 19);
+            this.radioButton6.TabIndex = 2;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "3";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.show_three_ball);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(29, 85);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(35, 19);
+            this.radioButton5.TabIndex = 1;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "2";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.show_two_ball);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(29, 39);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(35, 19);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "1";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.show_one_ball);
+            // 
+            // startbutton
+            // 
+            this.startbutton.Font = new System.Drawing.Font("新細明體", 20F);
+            this.startbutton.Location = new System.Drawing.Point(891, 80);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(156, 92);
+            this.startbutton.TabIndex = 6;
+            this.startbutton.Text = "GO";
+            this.startbutton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.startbutton.UseVisualStyleBackColor = true;
+            this.startbutton.Click += new System.EventHandler(this.startbutton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(639, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 34);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "得分:";
+            // 
+            // textbox_score
+            // 
+            this.textbox_score.Font = new System.Drawing.Font("新細明體", 20F);
+            this.textbox_score.Location = new System.Drawing.Point(737, 107);
+            this.textbox_score.Name = "textbox_score";
+            this.textbox_score.Size = new System.Drawing.Size(117, 47);
+            this.textbox_score.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1482, 953);
+            this.Controls.Add(this.textbox_score);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startbutton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.saber);
+            this.Controls.Add(this.ball3);
+            this.Controls.Add(this.ball2);
+            this.Controls.Add(this.ball1);
+            this.Name = "Form1";
+            this.Text = "E14031083_HW2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.saber)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox ball1;
+        private System.Windows.Forms.PictureBox ball2;
+        private System.Windows.Forms.PictureBox ball3;
+        private System.Windows.Forms.PictureBox saber;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.Button startbutton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textbox_score;
+    }
+}
+
---
+++ E14031083/Form1.cs
@@ -0,0 +1,341 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Media;
+
+namespace E14031083
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        SoundPlayer player = new SoundPlayer("...\\...\\ping.wav");
+        SoundPlayer end_sound = new SoundPlayer("...\\...\\endgame.wav");
+        SoundPlayer start_sound = new SoundPlayer("...\\...\\startsound.wav");
+        bool drag = false;
+        int myscore = 0;
+        int sx, sy;
+        bool ifstart = false;
+       const double speed1 = 20;
+        const double speed2 = 40;
+        const double speed3 = 60;
+        int speedsaber = 40;
+        double tempx;
+        double tempy;
+        int margin = 80;
+        int margin2 = 0;
+        double marginball = 120;
+        double ball1x = (speed1) * Math.Cos(30);
+        double ball1y = (speed1) * Math.Sin(30);
+        double ball2x = (speed1+10) * Math.Cos(45);
+        double ball2y = (speed1+10) * Math.Sin(45);
+        double ball3x = (speed1+20) * Math.Cos(60);
+        double ball3y = (speed1+20) * Math.Sin(60);
+
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+            ball1.Top = random.Next(90,250);
+            ball1.Left = random.Next(300, 500);
+
+            ball2.Top = random.Next(90, 200);
+            ball2.Left = random.Next(600,900);
+
+            ball3.Top = random.Next(300, 400);
+            ball3.Left = random.Next(300, 900);
+            
+            this.KeyPreview = true;  // 表單接收所有按鍵事件
+           this.FormBorderStyle = FormBorderStyle.Fixed3D;  // 表單大小固定
+            timer1.Enabled = false;	   
+            timer1.Interval = 100;
+            radioButton1.Checked = true;
+            radioButton4.Checked = true;
+            ball2.Visible = false;
+            timer1.Enabled = false;
+            ball3.Visible = false;
+            textbox_score.Text = myscore.ToString();
+            startbutton.Text = "GO";
+            myscore = 0;
+            textbox_score.Text = myscore.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (ball1.Right > this.Width - margin)
+            {
+                ball1x = -Math.Abs(ball1x);
+            }
+            if (ball1.Left < 0 + margin)
+            {
+                ball1x = Math.Abs(ball1x);
+            }
+            if (ball2.Right > this.Width - margin)
+            {
+                ball2x = -Math.Abs(ball2x);
+
+            }
+            if (ball2.Left < 0 + margin)
+            {
+                ball2x = Math.Abs(ball2x);
+            }
+            if (ball3.Right > this.Width - margin)
+            {
+
+                ball3x = -Math.Abs(ball3x);
+            }
+            if (ball3.Left < 0 + margin)
+            {
+                ball3x = Math.Abs(ball3x);
+            }
+
+            if (ball1.Top < margin)
+            {
+                ball1y = Math.Abs(ball1y);
+            }
+            if(ball1.Bottom > this.Height - margin)
+            {
+                ball1y = -Math.Abs(ball1y);
+            }
+
+            if (ball1.Bottom > this.Height - margin &&ball1.Visible==true)
+            {
+                ball1y = -Math.Abs(ball1y);
+                end_sound.Play();
+                timer1.Enabled = false;
+                ifstart = false;
+                MessageBox.Show("遊戲結束");
+                Form1_Load(sender, e);
+            }
+
+            if (ball2.Top < margin)
+            {
+                ball2y = Math.Abs(ball2y);
+
+            }
+
+            if (ball2.Bottom > this.Height - margin)
+            {
+                ball2y = -Math.Abs(ball2y);
+            }
+
+            if (ball2.Bottom > this.Height - margin && ball2.Visible==true)
+            {
+                ball2y = -Math.Abs(ball2y);
+                end_sound.Play();
+                timer1.Enabled = false;
+                ifstart = false;
+                MessageBox.Show("遊戲結束");
+                Form1_Load(sender, e);
+            }
+            if (ball3.Top < margin)
+            {
+                ball3y = Math.Abs(ball3y);
+            }
+
+            if (ball3.Bottom > this.Height - margin)
+            {
+                ball3y = -Math.Abs(ball3y);
+            }
+
+            if (ball3.Bottom > this.Height - margin && ball3.Visible == true)
+            {
+                ball3y = -Math.Abs(ball3y);
+                end_sound.Play();
+                timer1.Enabled = false;
+                ifstart = false;
+                MessageBox.Show("遊戲結束");
+                Form1_Load(sender, e);
+            }
+            double ball1centerx = (ball1.Left + ball1.Right) / 2;
+            double ball1centery = (ball1.Top + ball1.Bottom) / 2;
+            double ball2centerx = (ball2.Left + ball2.Right) / 2;
+            double ball2centery = (ball2.Top + ball2.Bottom) / 2;
+            double ball3centerx = (ball3.Left + ball3.Right) / 2;
+            double ball3centery = (ball3.Top + ball3.Bottom) / 2;
+            double distance12 = Math.Pow((ball1centerx - ball2centerx) * (ball1centerx - ball2centerx) + (ball1centery - ball2centery) * (ball1centery - ball2centery), 0.5);
+            double distance23 = Math.Pow((ball2centerx - ball3centerx) * (ball2centerx - ball3centerx) + (ball2centery - ball3centery) * (ball2centery - ball3centery), 0.5);
+            double distance31 = Math.Pow((ball3centerx - ball1centerx) * (ball3centerx - ball1centerx) + (ball3centery - ball1centery) * (ball3centery - ball1centery), 0.5);
+            if (ball2.Visible == true)
+            {
+                if (distance12 < marginball)
+                {
+                    tempx = ball1x;
+                    tempy = ball1y;
+                    ball1x = ball2x;
+                    ball1y = ball2y;
+                    ball2x = tempx;
+                    ball2y = tempy;
+                }
+            }
+            if (ball2.Visible == true && ball3.Visible == true)
+            {
+                if (distance23 < marginball)
+                {
+
+                    tempx = ball2x;
+                    tempy = ball2y;
+                    ball2x = ball3x;
+                    ball2y = ball3y;
+                    ball3x = tempx;
+                    ball3y = tempy;
+                }
+            }
+            if (ball3.Visible == true)
+            {
+                if (distance31 < marginball)
+                {
+                    tempx = ball3x;
+                    tempy = ball3y;
+                    ball3x = ball1x;
+                    ball3y = ball1y;
+                    ball1x = tempx;
+                    ball1y = tempy;
+                }
+            }
+            ball1.Left += Convert.ToInt32(ball1x);
+            ball1.Top += Convert.ToInt32(ball1y);
+            ball2.Left += Convert.ToInt32(ball2x);
+            ball2.Top += Convert.ToInt32(ball2y);
+            ball3.Left += Convert.ToInt32(ball3x);
+            ball3.Top += Convert.ToInt32(ball3y);      
+            if(ball1.Bottom>saber.Top +margin2 && ball1.Left>saber.Left && ball1.Right<saber.Right && ball1y>0 &&ball1.Visible==true)
+            {
+                ball1y = -Math.Abs(ball1y);
+                myscore++;
+                textbox_score.Text = myscore.ToString();
+                   player.Play();           
+            }
+            if (ball2.Bottom > saber.Top+margin2 && ball2.Left > saber.Left && ball2.Right < saber.Right &&ball2y>0&&ball2.Visible==true)
+            {
+                ball2y = -Math.Abs(ball2y);
+                myscore++;
+                textbox_score.Text = myscore.ToString();
+                  player.Play();
+              
+            }
+            
+            if (ball3.Bottom > saber.Top+margin2 && ball3.Left > saber.Left && ball3.Right < saber.Right &&ball3y>0 && ball3.Visible==true)
+            {
+                ball3y = -Math.Abs(ball3y);
+                myscore++;
+                textbox_score.Text = myscore.ToString();
+                   player.Play();            
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)        // 根據e.KeyCode分別執行
+            {
+                case Keys.Left:        // 若按向左鍵
+                    if (saber.Left > 0)
+                    {
+                        saber.Left -= speedsaber;  // 左移s點}
+                    }
+                    break;
+                case Keys.Right:       // 若按向右鍵
+                    if (saber.Right < this.Width)
+                    {
+                        saber.Left += speedsaber;  // 右移s點
+                    }
+                    break;
+            }
+        }
+
+        private void mouse_down(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)    // 若按左鍵時
+            {
+                drag = true;                 // 設為可拖曳
+                sx = e.X; sy = e.Y;          // 記錄滑鼠指標的座標値
+            }
+        }
+        private void slow_ckecked(object sender, EventArgs e)
+        {
+            ball1x = (speed1) * Math.Cos(30);
+             ball1y = (speed1) * Math.Sin(30);
+           ball2x = (speed1 + 10) * Math.Cos(45);
+             ball2y = (speed1 + 10) * Math.Sin(45);
+            ball3x = (speed1 + 20) * Math.Cos(60);
+           ball3y = (speed1 + 20) * Math.Sin(60);
+        }
+
+        private void medium_checked(object sender, EventArgs e)
+        {
+            ball1x = (speed2) * Math.Cos(30);
+            ball1y = (speed2) * Math.Sin(30);
+            ball2x = (speed2 + 10) * Math.Cos(45);
+            ball2y = (speed2 + 10) * Math.Sin(45);
+           ball3x = (speed2 + 20) * Math.Cos(60);
+            ball3y = (speed2 + 20) * Math.Sin(60);
+        }
+
+        private void fast_checked(object sender, EventArgs e)
+        {
+             ball1x = (speed3) * Math.Cos(30);
+             ball1y = (speed3) * Math.Sin(30);
+             ball2x = (speed3 + 10) * Math.Cos(45);
+             ball2y = (speed3 + 10) * Math.Sin(45);
+           ball3x = (speed3 + 20) * Math.Cos(60);
+            ball3y = (speed3 + 20) * Math.Sin(60);
+        }
+
+        private void show_one_ball(object sender, EventArgs e)
+        {
+            ball1.Visible = true;
+            ball2.Visible = false;
+            ball3.Visible = false;
+        }
+
+        private void show_two_ball(object sender, EventArgs e)
+        {
+            ball1.Visible = true;
+            ball2.Visible = true;
+            ball3.Visible = false;
+        }
+
+        private void show_three_ball(object sender, EventArgs e)
+        {
+            ball1.Visible = true;
+            ball2.Visible = true;
+            ball3.Visible = true;
+        }
+
+        private void startbutton_Click(object sender, EventArgs e)
+        {
+            if(ifstart==false)
+            {
+                start_sound.Play();
+                timer1.Enabled = true;
+                startbutton.Text = "STOP";
+                ifstart = true;
+            }
+            else
+            {
+                startbutton.Text = "GO";
+                timer1.Enabled = false;
+                ifstart = false;
+            }
+        }
+
+        private void mouse_move(object sender, MouseEventArgs e)
+        {
+            if (drag)   //若drag=true
+            {
+                saber.Left += (e.X - sx);   // 設圖片X位置                                       
+            }
+        }
+    }
+
+}
